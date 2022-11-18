8a9862a55a97e01d207f6661b2da4733abdb0136
rohan057 <116261091+rohan057@users.noreply.github.com>
2022-11-18 15:49:43 +0000

    Update products.blade.php
    Added references for images

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -66,73 +66,73 @@
 <div class="box-container"> <!-- creates the box for the image and text to be placed in -->
 
    <div class="product">
-      <img src="images/iphone.jpg" alt=""> <!--  -->
+      <img src="images/iphone.jpg" alt=""> <!-- https://www.apple.com/uk/newsroom/2022/09/apple-introduces-iphone-14-and-iphone-14-plus/ -->
       <h3>iPhone 14</h3>
       <button type="submit" onclick="myAlert()" class="btn">Add to Basket</button>
    </div>
 
    <div class="product">
-      <img src="images/hp.webp" alt=""> <!--  -->
+      <img src="images/hp.webp" alt=""> <!-- https://www.hp.com/gb-en/shop/product.aspx?id=6w7l7ea&opt=abu&sel=ntb -->
       <h3>HP Laptop</h3>
       <button type="submit" onclick="myAlert()" class="btn">Add to Basket</button>
    </div>
 
    <div class="product">
-      <img src="images/PS5.webp" alt=""> <!--  -->
+      <img src="images/PS5.webp" alt=""> <!-- https://direct.playstation.com/en-gb/buy-consoles/playstation5-console -->
       <h3>PS5</h3>
       <button type="submit" onclick="myAlert()" class="btn">Add to Basket</button>
    </div>
 
    <div class="product">
-      <img src="images/samsung.jpg" alt=""> <!--  -->
+      <img src="images/samsung.jpg" alt=""> <!-- https://www.costco.co.uk/TVs-Electronics-Security/Televisions-Projectors/Televisions/Samsung-UE58AU7100KXXU-58-Inch-4K-Ultra-HD-Smart-TV/p/394568 -->
       <h3>Samsung TV</h3>
       <button type="submit" onclick="myAlert()" class="btn">Add to Basket</button>
    </div>
 
    <div class="product">
-      <img src="images/acer.jpg" alt=""> <!--  -->
+      <img src="images/acer.jpg" alt=""> <!-- https://www.portusdigital.com/acer-predator--xb253q-gw-245--full-hd-wled-240hz-gaming-monitor-10164-p.asp -->
       <h3>Acer Monitor</h3>
       <button type="submit" onclick="myAlert()" class="btn">Add to Basket</button>
    </div>
 
    <div class="product">
-      <img src="images/airpods.jfif" alt=""> <!--  -->
+      <img src="images/airpods.jfif" alt=""> <!-- https://www.apple.com/uk/shop/product/MQD83ZM/A/airpods-pro -->
       <h3>Airpods Pro</h3>
       <button type="submit" onclick="myAlert()" class="btn">Add to Basket</button>
    </div>
 
    <div class="product">
-      <img src="images/ipad.jfif" alt=""> <!--  -->
+      <img src="images/ipad.jfif" alt=""> <!-- https://www.apple.com/uk/shop/product/FXAV2B/A/refurbished-129-inch-ipad-pro-wi-fi-512gb-space-gray-4th-generation -->
       <h3>iPad Pro</h3>
       <button type="submit" onclick="myAlert()" class="btn">Add to Basket</button>
    </div>
 
    <div class="product">
-      <img src="images/mac-book.jpg" alt=""> <!--  -->
+      <img src="images/mac-book.jpg" alt=""> <!-- https://www.apple.com/uk/newsroom/2020/05/apple-updates-13-inch-macbook-pro-with-magic-keyboard-double-the-storage-and-faster-performance/ -->
       <h3>MacBook Pro</h3>
       <button type="submit" onclick="myAlert()" class="btn">Add to Basket</button>
    </div>
 
    <div class="product">
-      <img src="images/apple-watch.jfif" alt=""> <!--  -->
+      <img src="images/apple-watch.jfif" alt=""> <!-- https://www.apple.com/uk/shop/product/FQKW2B/A/Refurbished-Apple-Watch-Series-3-GPS-38mm-Gold-Aluminum-Case-with-Pink-Sand-Sport-Band -->
       <h3>Apple Watch Series 3</h3>
       <button type="submit" onclick="myAlert()" class="btn">Add to Basket</button>
    </div>
 
    <div class="product">
-      <img src="images/samsung-flip.jpg" alt=""> <!--  -->
+      <img src="images/samsung-flip.jpg" alt=""> <!-- https://www.amazon.co.uk/Samsung-Smartphone-Android-Folding-Version/dp/B09FQBDHJH -->
       <h3>Samsung Galaxy Z Flip 3</h3>
       <button type="submit" onclick="myAlert()" class="btn">Add to Basket</button>
    </div>
 
    <div class="product">
-      <img src="images/x-box.jpg" alt=""> <!--  -->
+      <img src="images/x-box.jpg" alt=""> <!-- https://www.smythstoys.com/uk/en-gb/video-games-and-tablets/xbox-gaming/xbox-series-x-%7C-s/xbox-series-x-%7C-s-consoles/xbox-series-x-1tb-console/p/192012 -->
       <h3>Xbox Series X</h3>
       <button type="submit" onclick="myAlert()" class="btn">Add to Basket</button>
    </div>
 
    <div class="product">
-      <img src="images/nintendo-switch.jpg" alt=""> <!--  -->
+      <img src="images/nintendo-switch.jpg" alt=""> <!-- https://www.ign.com/games/nintendo-switch -->
       <h3>Nintendo Switch</h3>
       <button type="submit" onclick="myAlert()" class="btn">Add to Basket</button>
    </div>
