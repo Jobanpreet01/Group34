0bebefba873539c6609c928049c9409964eb89c9
rohan057 <116261091+rohan057@users.noreply.github.com>
2022-11-22 13:04:42 +0000

    Update contact.blade.php
    Fixed quick links

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -54,7 +54,7 @@
 
 <section class="heading-link">
    <h3>Contact Us</h3>
-   <p> <a href="index.php">home</a> / contact </p> <!-- links the home heading on the contact me page back to the home page -->
+   <p> <a href="{{url('/')}}">home</a> / contact </p> <!-- links the home heading on the contact me page back to the home page -->
 </section>
 
       <section class="contact-me">
@@ -123,10 +123,10 @@
 
       <div class="box">
          <h3>quick links</h3>
-         <a href="index.php" class="link">Home</a> <!-- allows the user to navigate through the pages using the links in the footer -->
-         <a href="products.php" class="link">Products</a>
-         <a href="contact.php" class="link">Contact Us</a>
-         <a href="basket.php" class="link">Basket</a>
+         <a href="{{url('/')}}" class = "link">Home</a>
+      <a href="{{url('/products')}}" class="link">Products</a>
+      <a href="{{url('/contact')}}" class = "link">Contact Us</a>
+      <a href="{{url('/basket')}}" class = "link"><i class="fa fa-shopping-basket" aria-hidden="true"></i>
       </div>
 
    </div>
