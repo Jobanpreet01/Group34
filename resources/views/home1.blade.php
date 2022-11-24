12def25074ac53e2331c71d4df5da4ef8be5cb44
rohan057 <116261091+rohan057@users.noreply.github.com>
2022-11-24 09:10:06 +0000

    Update home1.blade.php
    Added picture reference and fixed quick links

--- resources/views/home1.blade.php
+++ resources/views/home1.blade.php
@@ -95,7 +95,7 @@
       
       <div class="swiper-wrapper">
 
-         <section class="swiper-slide slide" style="background: url(images/Technology-Watch.jpg);"> <!-- https://unsplash.com/photos/lzh3hPtJz9c -->
+         <section class="swiper-slide slide" style="background: url(images/Technology-Watch.jpg);"> <!-- https://www.netscribes.com/technology-watch/ -->
             <div class="content">
                </div>
          </section>
@@ -124,10 +124,10 @@
 
       <div class="box">
          <h3>quick links</h3>
-         <a href="index.php" class="link">Home</a> <!-- allows the user to navigate through the pages using the links in the footer -->
-         <a href="products.php" class="link">Products</a>
-         <a href="contact.php" class="link">Contact Us</a>
-         <a href="basket.php" class="link">Basket</a>
+         <a href="{{url('/')}}" class = "link">Home</a>
+      <a href="{{url('/products')}}" class="link">Products</a>
+      <a href="{{url('/contact')}}" class = "link">Contact Us</a>
+      <a href="{{url('/basket')}}" class = "link"><i class="fa fa-shopping-basket" aria-hidden="true"></i>
       </div>
 
    </div>
