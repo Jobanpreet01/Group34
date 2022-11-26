aba153098f7c59938d0670a2fcd2c62f5161d964
Amit Rahaman <210158742@aston.ac.uk>
2022-11-26 22:19:58 +0000

    added layout for adminhomepage tables and text

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -17,7 +17,7 @@ class AdminSeeder extends Seeder
     public function run()
     {
 
-    /**
+    
        $users = [
             [
                'name'=>'Admin',
@@ -30,11 +30,11 @@ class AdminSeeder extends Seeder
         foreach ($users as $key => $user) {
             User::create($user);
         }
-        */
-
+        
+/**
         $products = [
             [
-               'Title'=>'IPhone 14',
+               'Title'=>'Phone 14',
                'Price'=> 1000,
                'Description'=>'This Iphone is unique',
                'Quantity'=> 20,
@@ -42,23 +42,23 @@ class AdminSeeder extends Seeder
             ],
 
             [
-               'Title'=>'HP Laptop',
+               'Title'=>'HPST Laptop',
                'Price'=> 1200,
-               'Description'=>'This Laptop can run GTA5',
+               'Description'=>'This Laptop can run Games',
                'Quantity'=> 8,
                'Image'=>'images/hp.webp',
             ],
 
             [
-               'Title'=>'PS5',
+               'Title'=>'PSGame5',
                'Price'=> 850,
-               'Description'=>'This PS5 is Fast',
+               'Description'=>'This PSGame5 is Fast',
                'Quantity'=> 0,
                'Image'=>'images/PS5.webp',
             ],
 
             [
-               'Title'=>'Samsung TV',
+               'Title'=>'Samsun TV',
                'Price'=> 490,
                'Description'=>'This TV is Great',
                'Quantity'=> 12,
@@ -69,6 +69,8 @@ class AdminSeeder extends Seeder
         foreach ($products as $key => $product) {
             Products::create($product);
         }
+
+        */
 
     }
 }
