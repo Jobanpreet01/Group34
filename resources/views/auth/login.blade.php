6168ce0fbefdc496ab96f0eda0fa385b041c46bb
Jobanpreet Singh <210142187@aston.ac.uk>
2022-11-29 22:21:09 +0000

    Added missing footer in the login.blade.php page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,12 +52,14 @@
       <a href="{{url('/')}}">Home</a>
       <a href="{{url('/products')}}" class="link">Products</a>
       <a href="{{url('/contact')}}">Contact Us</a>
+      <a href="{{url('/about')}}">About Us</a>
       <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
    </nav>
 
 </header>
 
-
+<br>
+<br>
 
 <main class="py-4">
 
@@ -140,7 +142,43 @@
 
 <!-- header section ends -->
 
+<!-- footer section starts  -->
 
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+      <h3>Vitality Electronics</h3>
+         <p>Welcome to our e-commerce website where we sell a large range of tech products. This company has been going for 15 years and we are proud to have over 100 million users and we are located all over the world with many connections in all parts of the world, which has allowed for us to expand globally and a fast rate. We look forward to you shopping with us and if you have any queries please feel free to find our contact details under the contacts section.</p>
+         <div class="share">
+
+         
+</div>
+      </div>
+
+      <div class="box">
+         <h3>Quick Links</h3>
+
+         <a href="{{url('/')}}" class = "link">Home</a>
+      <a href="{{url('/products')}}" class="link">Products</a>
+      <a href="{{url('/contact')}}" class = "link">Contact Us</a>
+      <a href="{{url('/about')}}" class = "link">About Us</a>
+
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>Group 34</span></div>
+
+   </section>
 
 
 </body>
